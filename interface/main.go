3f346cc4174f36c6aae9fe84cf26ff21f744f9a4
kwsk01 <kawasakitaiki0308@gmail.com>
2022-08-06 03:46:30 +0900

    commit

--- interface/main.go
+++ interface/main.go
@@ -1,81 +1,120 @@
 package main
 import (
 	"fmt"
-	"strings"
-	"strconv"
+	// "strings"
+	// "strconv"
 )
 
-//Data is interface for Mydata
-type Data interface {
-	SetValue(vals map[string]string)
-	PrintData()
+
+//General is all type data
+type General interface{}
+
+//GData is holding General vale
+type GData interface {
+	Set(nm string, g General)
+	Print()
 }
 
-// Mydata is structure
-type Mydata struct {
+//GDataImpl is structre
+type GDataImpl struct {
 	Name string
-	Data []int
-}
-//SetValue is Mydata method 
-func (md *Mydata) SetValue(vals map[string]string) {
-	md.Name = vals["name"]
-	valt := strings.Split(vals["data"], " ")
-	vali := []int{}
-	for _, i := range valt {
-		n, _ := strconv.Atoi(i)
-		vali = append(vali, n)
-	}
-	md.Data = vali
+	Data General
 }
 
-//PrintData is Mydata method
-func (md *Mydata) PrintData(){
-	fmt.Println("Name:", md.Name)
-	fmt.Println("Data:", md.Data)
+//Set is GDataImpl method
+func (gd *GDataImpl) Set(nm string, g General) {
+	gd.Name = nm
+	gd.Data = g
 }
 
-//Yourdata is structure
-type Yourdata struct {
-	Name string
-	Mail string
-	Age int
-}
-
-//SetValu is Yourdata method
-func (md *Yourdata) SetValue(vals map[string]string){
-	md.Name = vals["name"]
-	md.Mail = vals["mail"]
-	n, _ := strconv.Atoi(vals["age"])
-	md.Age = n
-}
-
-//PrintData is Yourdata method
-func (md *Yourdata)PrintData() {
-	fmt.Printf("I'm %s. (%d).\n",md.Name,md.Age)
-	fmt.Printf("mail:%s.\n",md.Mail)
+//print is GDataImpl method
+func (gd *GDataImpl) Print(){
+	fmt.Printf("<<%s>>",gd.Name)
+	fmt.Println(gd.Data)
 }
 
 func main() {
-	ob := [2]Data{}
-
-	ob[0] = new(Mydata)
-	ob[0].SetValue(map[string]string{
-		"name":"taiki",
-		"data":"23,44,12",
-	})
-
-	ob[1] = new(Yourdata)
-	ob[1].SetValue(map[string]string{
-		"name":"taro",
-		"mail":"taro@gmail.wo",
-		"aga": "22",
-	})
-
-	for _, d := range ob {
-		d.PrintData()
-		fmt.Println()
+	var data = []GDataImpl{}
+	data = append(data, GDataImpl{"taiki",123})
+	data = append(data, GDataImpl{"kawasaki","hello"})
+	data = append(data, GDataImpl{"Go",[]int{34,3,55}})
+	for _, ob := range data {
+		ob.Print()
 	}
 }
+
+//--------------------------------------
+// //Data is interface for Mydata
+// type Data interface {
+// 	SetValue(vals map[string]string)
+// 	PrintData()
+// }
+
+// // Mydata is structure
+// type Mydata struct {
+// 	Name string
+// 	Data []int
+// }
+// //SetValue is Mydata method 
+// func (md *Mydata) SetValue(vals map[string]string) {
+// 	md.Name = vals["name"]
+// 	valt := strings.Split(vals["data"], " ")
+// 	vali := []int{}
+// 	for _, i := range valt {
+// 		n, _ := strconv.Atoi(i)
+// 		vali = append(vali, n)
+// 	}
+// 	md.Data = vali
+// }
+
+// //PrintData is Mydata method
+// func (md *Mydata) PrintData(){
+// 	fmt.Println("Name:", md.Name)
+// 	fmt.Println("Data:", md.Data)
+// }
+
+// //Yourdata is structure
+// type Yourdata struct {
+// 	Name string
+// 	Mail string
+// 	Age int
+// }
+
+// //SetValu is Yourdata method
+// func (md *Yourdata) SetValue(vals map[string]string){
+// 	md.Name = vals["name"]
+// 	md.Mail = vals["mail"]
+// 	n, _ := strconv.Atoi(vals["age"])
+// 	md.Age = n
+// }
+
+// //PrintData is Yourdata method
+// func (md *Yourdata)PrintData() {
+// 	fmt.Printf("I'm %s. (%d).\n",md.Name,md.Age)
+// 	fmt.Printf("mail:%s.\n",md.Mail)
+// }
+
+// func main() {
+// 	ob := [2]Data{}
+
+// 	ob[0] = new(Mydata)
+// 	ob[0].SetValue(map[string]string{
+// 		"name":"taiki",
+// 		"data":"23,44,12",
+// 	})
+
+// 	ob[1] = new(Yourdata)
+// 	ob[1].SetValue(map[string]string{
+// 		"name":"taro",
+// 		"mail":"taro@gmail.wo",
+// 		"aga": "22",
+// 	})
+
+// 	for _, d := range ob {
+// 		d.PrintData()
+// 		fmt.Println()
+// 	}
+// }
 
 
 // //Data is interface
@@ -108,8 +147,3 @@ func main() {
 // 	ob.PrintData()
 // }
 
-// // func main () {
-// // 	var ob Mydata = Mydata{}
-// // 	ob.Initial("Taiki", []int{11,22,33})
-// // 	ob.PrintData()
-// // }
--- pointer/main.go
+++ pointer/main.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 )
 
-func main() {
-	ar := []int{10,20,30}
-	fmt.Println(ar)
-	initial(&ar)
-	fmt.Println(ar)
-}
+// func main() {
+// 	ar := []int{10,20,30}
+// 	fmt.Println(ar)
+// 	initial(&ar)
+// 	fmt.Println(ar)
+// }
 
 func initial(ar *[]int){
 	for i := 0; i < len(*ar); i++ {
@@ -50,3 +50,37 @@ func initial(ar *[]int){
 // 	fmt.Printf("p value:%d, address:%p\n",**q,q)
 // 	fmt.Printf("p2 value:%d, address:%p\n", **q2, q2)
 // }
+
+
+// var 変数 *型 = &値
+// ポインタ型　アドレスを代入
+// 変数の前に&をつけることでアドレスの値を取り出せる
+//値を取り出す時[*]をつける
+
+
+// func main() {
+// 	n := 123
+// 	p := &n
+// 	fmt.Println("number",n)
+// 	fmt.Println("pointer",p)
+// 	fmt.Println("value",*p)
+// }
+
+//ポインタを引数に使う
+
+func main() {
+	n := 123
+	fmt.Printf("value:%d.\n",n)
+	change1(n)
+	fmt.Printf("value:%d.\n",n)
+	change2(&n)
+	fmt.Printf("value:%d.\n",n)
+}
+
+func change1(n int) {
+	n *= 2
+}
+
+func change2(n *int) {
+	*n *= 2
+}
