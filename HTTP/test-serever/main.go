07fa0e7aad331c8624539d466ef3ff05ad647173
kwsk01 <kawasakitaiki0308@gmail.com>
2022-08-06 04:25:42 +0900

    commit

---
+++ HTTP/test-serever/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	dump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(string(dump))
+	fmt.Fprintf(w, "<html><body>hello</body></html>\n")
+}
+
+func main() {
+	var httpServer http.Server
+	http.HandleFunc("/", handler)
+	log.Println("start http listening: 18888")
+	httpServer.Addr = ":18888"
+	log.Println(httpServer.ListenAndServe())
+}
+
+
+//HTTP/0.9を疑似的に試す
+//curl --http1.0 http://localhost:18888/greeting
+
+//HTTP/1.0
+//curl -v --http1.0 http://localhost:18888/greeting
