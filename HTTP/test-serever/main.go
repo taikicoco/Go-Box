7f296c7786eff48dcc290dc60ac889d418bb4046
kwsk01 <kawasakitaiki0308@gmail.com>
2022-08-07 00:31:56 +0900

    commit

--- HTTP/test-serever/main.go
+++ HTTP/test-serever/main.go
@@ -27,7 +27,10 @@ func main() {
 
 
 //HTTP/0.9を疑似的に試す
-//curl --http1.0 http://localhost:18888/greeting
+//$ curl --http1.0 http://localhost:18888/greeting
 
 //HTTP/1.0
-//curl -v --http1.0 http://localhost:18888/greeting
+//$ curl -v --http1.0 http://localhost:18888/greeting
+
+//ヘッダーの送信
+//$ curl --http1.0 -H "X-Test:Hello" http://localhost:18888/greeting
