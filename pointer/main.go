0d63ae393ec59fa434e328eb8c5d59ab7b0f2857
taikicoco <kawasakitaiki0308@gmail.com>
2023-02-26 02:30:16 +0900

    commit

--- pointer/main.go
+++ pointer/main.go
@@ -4,19 +4,47 @@ import (
 	"fmt"
 )
 
-func failedUpdate(px *int) {
-	x2 := 20
-	px = &x2
-}
-
-func update(px *int) {
-	*px = 20
-}
-
 func main() {
+	var x int32 = 10
+	var y bool = true
+	pointerX := &x
+	pointerY := &y
+	var pointerZ *string
+
+	x := "hello"
+	pointerToX := &x // ポインタ型
+
+	// 「*」は間接参照
+	// ポインタ型変数の前に付けると、そのポインタが参照するアドレスに保存されている値を返す
+	// これをデリファレンスと呼ぶ
 	x := 10
-	failedUpdate(&x)//10
-	fmt.Println(x)
-	update(&x)
-	fmt.Println(x) //20
+	pointerToX := &x
+	fmt.Println(pointerToX) // アドレスを表示
+	fmt.Println(*pointerToX) // 10 // デリファレンスする
+
+	var y *int
+	fmt.Println(y == nil) // true
+	fmt.Println(*y) // パニックになる
+
 }
+
+
+
+// 値渡しと参照渡し
+
+// func failedUpdate(px *int) {
+// 	x2 := 20
+// 	px = &x2
+// }
+
+// func update(px *int) {
+// 	*px = 20
+// }
+
+// func main() {
+// 	x := 10
+// 	failedUpdate(&x)
+// 	fmt.Println(x) // 10
+// 	update(&x)
+// 	fmt.Println(x) // 20
+// }
