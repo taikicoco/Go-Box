f67961f4788b7d1bfb7fea329590dbec277382c8
taikicoco <kawasakitaiki0308@gmail.com>
2024-01-06 20:23:38 +0900

    Refactor goroutine code to generate a lucky number

--- src/goroutine/main.go
+++ src/goroutine/main.go
@@ -2,45 +2,27 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"math/rand"
 	"time"
 )
 
-func main() {
-	msg := "start"
-	prmsg := func (nm string, n int) {
-		fmt.Println(nm, msg)
-		time.Sleep(time.Duration(n) * time.Microsecond)
-	}
-	hello := func(n int) {
-		const nm string = "hello"
-		for i := 0; i < 10; i++ {
-			msg += " h" + strconv.Itoa(i)
-			prmsg(nm, n)
-		}
-	}
-	main := func(n int) {
-		const nm string ="*main"
-		for i := 0; i < 5; i++ {
-			msg += " m" + strconv.Itoa(i)
-			prmsg(nm, 100)
-		}
-	}
-	go hello(60)
-	main(100)
+func getLuckyNum(c chan<- int) {
+	fmt.Println("...")
+	time.Sleep(3 * time.Second)
+
+	num := rand.Intn(10)
+	c <- num
 }
 
+func main() {
+	fmt.Println("what is today's lucky number?")
 
+	c := make(chan int)
+	go getLuckyNum(c)
 
-//--------------------------
-// func hello(s string, n int) {
-// 	for i := 1; i <= 10; i++ {
-// 		fmt.Printf("<%d %s>", i, s)
-// 		time.Sleep(time.Duration(n) * time.Millisecond)
-// 	}
-// }
+	num := <-c
 
-// func main() {
-// 	go hello("hello", 50)
-// 	hello("bye!", 100)
-// }
+	fmt.Printf("Today's your lucky number is %d!\n", num)
+
+	close(c)
+}
